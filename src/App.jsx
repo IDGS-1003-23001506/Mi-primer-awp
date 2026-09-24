a7e5cc17b1f5339cc985151a29cf20fb2435c177
IDGS-80223001506 <mi.hernan672@gmail.com>
2026-09-24 17:47:21 -0600

    Retiro texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.jsx'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import PWABadge from './PWABadge.jsx'
+import appLogo from '/favicon.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,9 +22,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
